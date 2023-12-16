0528f76c5409f78dc3236e213b02e3d9de1ff697
amine Chatate <amine.chatate@gmail.com>
2023-12-16 11:28:54 +0100

    create API Request

--- RickMorty/APIClient/RMRequest.swift
+++ RickMorty/APIClient/RMRequest.swift
@@ -9,4 +9,43 @@ import Foundation
 
 final class RMRequest {
     
+    private let endpoint: RMEndpoint
+    private let pathComponents: [String]
+    private let queryParameters: [URLQueryItem]
+    
+    private var urlString: String {
+        var string = Constants.BASE_URL
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach {
+                string += "/\($0)"
+            }
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap {
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }.joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    public let httpMethod = "GET"
+    
+    init(endpoint: RMEndpoint, pathComponents: [String] = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
---
+++ RickMorty/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  RickMorty
+//
+//  Created by Amine CHATATE on 16/12/2023.
+//
+
+import Foundation
+
+
+final class Constants {
+    
+    public static let BASE_URL = "https://rickandmortyapi.com/api"
+    
+}
